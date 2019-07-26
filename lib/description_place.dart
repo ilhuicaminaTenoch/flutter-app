4d9fd61836f386c25526e500511ee4f4d5fb58ee
Manijas <zerocool.moreno@gmail.com>
2019-07-25 19:05:42 -0500

    Se sube cambio para dos columnas

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -16,13 +16,29 @@ class DescriptionPlace extends StatelessWidget{
       ),
     );
 
+    final description =Container(
+          margin: EdgeInsets.only(
+              top: 10.0,
+              left: 20.0,
+              right: 20.0
+          ),
+          child: Text(
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                fontSize: 15.0,
+                letterSpacing: 0.5
+            ),
+          ),
+    );
+
     final title_starts = Row(
       children: <Widget>[
         Container(
           margin: EdgeInsets.only(
-            top: 320.0,
-            left: 20.0,
-            right: 20.0
+              top: 320.0,
+              left: 20.0,
+              right: 20.0
           ),
           child: Text(
             'Duwilli Ella',
@@ -39,13 +55,20 @@ class DescriptionPlace extends StatelessWidget{
             start,
             start,
             start,
-            start
+            start,
           ],
-        )
+        ),
+
       ],
+
     );
 
-    return title_starts;
+    return Column(
+      children: <Widget>[
+        title_starts,
+        description
+      ],
+    );
   }
 
 }
